5c2f66b4c51e06929444c57cd900f471000c89f1
Ardit Zubaku <zubaku92@gmail.com>
2026-08-07 02:56:37 +0200

    Load file from argv into buffer, add Ctrl+Q to quit

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"log/slog"
 	"os"
 
-	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
 
@@ -12,12 +13,10 @@ var (
 	ROWS, COLS       int
 	offsetX, offsetY int
 	textBuf          [][]rune
+	sourceFile       string
 )
 
 func main() {
-	textBuf = append(textBuf, []rune{'h', 'e', 'l', 'l', 'o'})
-	textBuf = append(textBuf, []rune{'w', 'o', 'r', 'l', 'd'})
-
 	runEditor()
 }
 
@@ -25,6 +24,15 @@ func runEditor() {
 	if err := termbox.Init(); err != nil {
 		slog.Error("Could not init termbox", "error", err)
 		os.Exit(1)
+	}
+
+	if len(os.Args) > 1 {
+		sourceFile = os.Args[1]
+		readFile(sourceFile)
+	} else {
+		sourceFile = "out.txt"
+		// So we have a new line at the end of the file
+		textBuf = append(textBuf, []rune{})
 	}
 
 	for {
@@ -49,18 +57,10 @@ func runEditor() {
 		}
 
 		e := termbox.PollEvent()
-		if e.Type == termbox.EventKey && e.Key == termbox.KeyEsc {
+		if e.Type == termbox.EventKey && (e.Key == termbox.KeyEsc || e.Key == termbox.KeyCtrlQ) {
 			termbox.Close()
 			break
 		}
-	}
-}
-
-// nolint
-func printMessage(col, row int, fg, bg termbox.Attribute, msg string) {
-	for _, ch := range msg {
-		termbox.SetCell(col, row, ch, fg, bg)
-		col += runewidth.RuneWidth(ch)
 	}
 }
 
@@ -96,5 +96,44 @@ func displayTextBuffer() {
 				termbox.SetChar(col, row, ch)
 			}
 		}
+	}
+}
+
+func readFile(name string) {
+	file, err := os.Open(name)
+	if err != nil {
+		sourceFile = name
+		textBuf = append(textBuf, []rune{})
+		return
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil && !errors.Is(err, os.ErrClosed) {
+			slog.Error("Failed to close file", "path", file.Name(), "error", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		textBuf = append(textBuf, []rune{})
+
+		for i := 0; i < len(line); i++ {
+			textBuf[lineNum] = append(textBuf[lineNum], rune(line[i]))
+		}
+
+		lineNum++
+	}
+
+	if err := scanner.Err(); err != nil {
+		slog.Error("Failed to scan the file", "file", file.Name(), "error", err)
+		// TODO: Show the user something about this
+		return
+	}
+
+	if lineNum == 0 {
+		textBuf = append(textBuf, []rune{})
 	}
 }
