9be2f76a5a6570383d2715faa5bc6d1926f56a5d
Ardit Zubaku <zubaku92@gmail.com>
2026-08-07 12:31:55 +0200

    Add arrow key and VIM-motion cursor navigation

---
+++ navigation.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/nsf/termbox-go"
+)
+
+func processKeyPress() {
+	keyEvent := getKey()
+
+	if keyEvent.Key == termbox.KeyEsc {
+		termbox.Close()
+		os.Exit(0)
+	} else if keyEvent.Ch != 0 {
+		// handle characters
+		fmt.Printf("here %+v", keyEvent)
+
+		// NOTE: VIM motions for scrolling
+		switch keyEvent.Ch {
+		case 104: // h <left>
+			left()
+		case 106: // j <down>
+			down()
+		case 107: // k <up>
+			up()
+		case 108: // l <right>
+			right()
+		case 117: // u <up a page>
+			if currentRow != 0 {
+				currentRow -= 15 // idk how many lines VIM goes up but this should be good
+			}
+		case 100: // d <down a page>
+			if currentRow < len(textBuf)-1 {
+				currentRow += 15
+			}
+		}
+	} else {
+		switch keyEvent.Key {
+		case termbox.KeyArrowUp:
+			up()
+		case termbox.KeyArrowDown:
+			down()
+		case termbox.KeyArrowLeft:
+			left()
+		case termbox.KeyArrowRight:
+			right()
+		}
+	}
+}
+
+func up() {
+	if currentRow != 0 {
+		currentRow--
+	}
+}
+
+func down() {
+	if currentRow < len(textBuf)-1 {
+		currentRow++
+	}
+}
+
+func left() {
+	if currentCol != 0 {
+		currentCol--
+	} else if currentRow > 0 {
+		// if we are not on the first line
+		// move to the end of the previous line
+		currentRow--
+		currentCol = len(textBuf[currentRow])
+	}
+}
+
+func right() {
+	lineLen := len(textBuf[currentRow])
+	if currentCol < lineLen {
+		currentCol++
+	} else if currentRow < len(textBuf)-1 {
+		// if we are not on the last line
+		// move to the first column of a the new line
+		currentRow++
+		currentCol = 0
+	}
+}
